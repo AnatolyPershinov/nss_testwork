a4b2a7dc5a65e5b80a8400dccb73953d0181d534
Anatoly Pershinov <anatolii.pershinov@sparm.com>
2022-09-29 17:38:01 +0300

    finished parser for first file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # read excel file to object. send excel object to handler. save data from handler in csv
 from parser1 import main as handler1
 
+
 def main():
     workbook_parsers = {
         r"Задание на парсер\1.xlsx" : handler1,
@@ -9,8 +10,6 @@ def main():
     for path, func in workbook_parsers.items():
         print(path)
         func(path)
-
-
 
 
 if __name__ == "__main__":
--- parser1.py
+++ parser1.py
@@ -1,44 +1,61 @@
-from openpyxl import load_workbook
+import csv
+
+from typing import List
 from dataclasses import dataclass
+from openpyxl import load_workbook
 
 
 @dataclass
 class Work:
     index: int
     name: str
-    plan_shedule: list
-    fact_shedule: list
+    plan_schedule: list
+    fact_schedule: list
 
+    def get_dict(self):
+        result = {"Название работы": self.name}
+        header = [x for x in range(1, len(self.plan_schedule)+1)]
+        result.update(dict(zip([str(day) +" plan" for day in header], self.plan_schedule)))
+        result.update(dict(zip([str(day) +" fact" for day in header], self.fact_schedule)))
+        return result
 
 @dataclass
 class Resource:
     index: int
     name: str
-    shedule: list
+    schedule: list
+    
+    def get_dict(self):
+        result = {"Название ресурса": self.name}
+        header = [x for x in range(1, len(self.schedule)+1)]
+        result.update(dict(zip([str(day) for day in header], self.schedule)))
+        return result
 
 
-def parser(sheet, days : int):
+def parser(sheet, days : int) ->  tuple[list[Work], list[Resource]]:
     # collect information about works and resources
-    works: list(Work) = []
-    resources: list(Resource) = []
+    works: List[Work] = []
+    resources: List[Resource] = []
 
     # part1. parse works
-    # work rows contain cell with value "план".   
+    # work rows contain cell with value "план"   
     for cell in sheet["S"]:
-        if cell.value == "план":
-            row = sheet[cell.row]
-            fact_row = sheet[cell.row+1] # row with fact activity
+        if cell.value != "план":
+            continue
 
-            # create lists of shedules
-            plan_shedule = [c.value for c in row[19:19+days]]
-            fact_shedule = [c.value for c in fact_row[19:19+days]]
-            works.append(Work(
-                index=len(works),
-                name=row[1].value+"act",
-                plan_shedule=[0 if v is None else v for v in plan_shedule], # replace none to 0
-                fact_shedule=[0 if v is None else v for v in fact_shedule]
-            ))
-    
+        row = sheet[cell.row]
+        fact_row = sheet[cell.row+1] # row with fact activity
+
+        # create lists of schedules
+        plan_schedule = [c.value for c in row[19:19+days]]
+        fact_schedule = [c.value for c in fact_row[19:19+days]]
+        works.append(Work(
+            index=len(works),
+            name=row[1].value+"_act",
+            plan_schedule=[0 if v is None else v for v in plan_schedule], # replace none to 0
+            fact_schedule=[0 if v is None else v for v in fact_schedule]
+        ))
+        
     # part2. parse resources
     start_pos = 0
     end_pos = 0
@@ -52,29 +69,33 @@ def parser(sheet, days : int):
             break
     
     for cell in sheet["B"][start_pos:end_pos]:
-        if cell.value != None and cell.value != "Наименование должностей, профессий":
+        if cell.value is not None and cell.value != "Наименование должностей, профессий":
             resource_rows.append(sheet[cell.row])
 
     for cell in sheet["B"][end_pos+6:]:
-        if cell.value != None and cell.value != "Наименование субподрядной организации" and cell.value != 2:
+        if cell.value is not None and cell.value != "Наименование субподрядной организации" and cell.value != 2:
             resource_rows.append(sheet[cell.row])
 
-    print(resource_rows)
-
     for resource in resource_rows:
-        plan_shedule = [c.value for c in resource[19:19+days]]
+        plan_schedule = [c.value for c in resource[19:19+days]]
         
         resources.append(Resource(
             index=len(resources),
             name=resource[1].value+"_res", 
-            shedule=[0 if v is None else v for v in plan_shedule]
+            schedule=[0 if v is None else v for v in plan_schedule]
         ))
 
     return works, resources
 
 
-def save(array, filename):
-    pass
+def save_to_csv(filename, array):
+    with open(filename, "w", encoding="UTF-8", newline='\r\n') as csvfile:
+        header = array[0].get_dict().keys()
+        writer = csv.DictWriter(f=csvfile, fieldnames=header, lineterminator="\n")
+        writer.writeheader()
+        for k in array:
+            writer.writerow(k.get_dict())
+
 
 def main(path: str):
     workbook = load_workbook(path)
@@ -84,4 +105,7 @@ def main(path: str):
 
     for month in months:
         ws = workbook.get_sheet_by_name(month["name"])
-        parser(ws, days=month["days"])
+        works, resources = parser(ws, days=month["days"])
+        save_to_csv("activites.csv", works)
+        save_to_csv("resources.csv", resources)
+
